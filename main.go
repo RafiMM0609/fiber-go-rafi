f3761c56a04815d43ece200d3a4dbdbe0018c0d4
rafimahrus <mahrusrafi@gmail.com>
2025-11-01 13:07:18 +0700

    done tipe data

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package fiber_go
+package main
 
 import "fmt"
 
@@ -15,9 +15,18 @@ var tinggi_badan int
 var sudah_makan bool
 var pnl float64
 
+func printVariabelTanpaValue(){
+	fmt.Println("Variabel tanpa value:")
+	fmt.Println(kota)
+	fmt.Println(tinggi_badan)
+	fmt.Println(sudah_makan)
+	fmt.Println(pnl)
+}
+
 // deklarasi variabel singkat * hanya bisa didalam fungsi
 
 func DeklarasiVariabel(){
+	fmt.Println("Deklarasi variabel:")
 	kodepos := 546321
 	berat_badan := 70.5
 	pesan_pengguna := "Selamat datang di golang"
@@ -29,8 +38,15 @@ func DeklarasiVariabel(){
 
 
 // deklarasi konstanta
+
 const phi = 3.14
 const nama_wali = "Rafi Mahrus"
+
+func printKonstanta(){
+	fmt.Println("Print Konstanta:")
+	fmt.Println(phi)
+	fmt.Println(nama_wali)
+}
 
 // konversi
 
@@ -39,11 +55,19 @@ var b int32 = 32
 
 var c int = int(b)
 
+func printKonversi(){
+	fmt.Println("Print konversi :")
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
 
 // string di go adalah urutan byte
 // string immutable (tidak bisa diubah)
 
 func CobaString(){
+	fmt.Println("Coba String:")
 	s1 := "Rafi"
 	fmt.Println(s1[0])
 
@@ -59,8 +83,21 @@ func getMessage(a bool) string {
 }
 
 func KondisionalString() {
+	fmt.Println("Kondisional String:")
 	a := false
 
 	message := getMessage(a)
 	fmt.Println(message)
+}
+
+func main() {
+	fmt.Println(nama_lengkap)
+	fmt.Println(usia)
+	fmt.Println(ipk)
+	fmt.Println(pintar)
+	printVariabelTanpaValue()
+	printKonstanta()
+	printKonversi()
+	CobaString()
+	KondisionalString()
 }
